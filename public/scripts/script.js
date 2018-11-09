f3430e8cfcdcf0432c39f7f76708755683613a8a
Jake Lewochko <jlewochko@gmail.com>
2018-11-09 13:08:45 -0500

    reorganized code

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,158 +1,94 @@
 // to do
-// fix transitions from recorded preview back to live view
-// add replay button
-// add preview button for snapshots
-// add transition from still preview to live view
 
-$(document).ready(() => {
-  $('#fullscreen-btn').find('path:even').hide()
-  $('#pause').hide()
-  $('#play').hide()
-  $('#local-video')[0].onended = function () {
-    $('#play').show('slow')
-    $('#pause').hide('slow')
-  }
-  jQuery.fn.rotate = function (deg) {
-    $(this).css({
-      '-webkit-transform': 'rotate(' + deg + 'deg)',
-      '-moz-transform': 'rotate(' + deg + 'deg)',
-      '-ms-transform': 'rotate(' + deg + 'deg)',
-      transform: 'rotate(' + deg + 'deg)'
-    })
-    return $(this)
-  }
-  function disableToggle (btn, op) {
-    $(btn).prop('disabled', op)
-  }
-  function downloadLinkSetup (url, ext, type) {
-    $('#download-btn')
-      .html(`Download ${type}`)
-      .prop('href', url)
-      .prop('download', `test.${ext}`)
-      .show('slow')
-  }
-  $('#download-btn').click(function () {
-    $(this).hide('slow')
-    if ($('#local-video').is(':hidden')) {
-      $('#preview-canvas').hide()
-      $('#local-video').show()
-    } else {
-      $('#local-video').prop('srcObject', window.stream)
-    }
-  })
-  function setupRecorder () {
+// add a progressBar
+const recorder = {
+  init: () => {
     window.recordedBlobs = []
     window.recorder = new MediaRecorder(window.stream)
     window.recorder.ondataavailable = blob => {
       window.recordedBlobs.push(blob.data)
     }
+    window.recorder.onstart = () => {
+      $('#stop').show('slow')
+      $('#rec').hide('slow')
+      $('#pause').show('slow')
+      $('#snap-btn').show('slow')
+      $('#cam-btn').hide('slow')
+    }
+    window.recorder.onstop = () => {
+      $('#stop').hide()
+      $('#pause').attr('fill', '#fff').attr('stroke', '#fff')
 
-    window.recorder.onstop = e => {
       const url = window.URL.createObjectURL(
         new Blob(window.recordedBlobs, { type: 'video/webm' })
       )
       $('#local-video').prop('srcObject', null).attr('src', url)
       window.recordedBlobs = []
-      downloadLinkSetup(url, 'webm', 'Video')
+      controls.setupDownloadLink(url, 'webm', 'Video')
+    }
+    window.recorder.onpause = () => {
+      $('#pause').attr('fill', '#0095ff').attr('stroke', '#0095ff')
+    }
+    window.recorder.onresume = () => {
+      $('#pause').attr('fill', '#fff').attr('stroke', '#fff')
     }
   }
+}
+const stream = {
+  init: () => {
+    return navigator.mediaDevices
+      .getUserMedia({
+        video: true
+      })
+      .then(stream => {
+        window.stream = stream
+        $('#local-video').prop('srcObject', window.stream)
+      })
+      .catch(console.log)
+  },
 
-  function handleError (err) {
-    console.log(err)
+  enableStream: () => {
+    window.stream.getVideoTracks()[0].enabled = true
+    $('#cam-btn').html('Hide Cam')
+    $('#fullscreen-btn').show('slow')
+    $('#rec').show('slow')
+  },
+  disableStream: () => {
+    window.stream.getVideoTracks()[0].enabled = false
+    $('#cam-btn').html('Show Cam')
+    $('#snap-btn').hide('slow')
+    $('#rec').hide('slow')
+    $('#fullscreen-btn').hide('slow')
   }
+}
 
-  $('#fullscreen-btn').click(function (e) {
-    let video = $('.video')[0]
-
-    if ($(this).attr('fill') === '#fff') {
-      if (video.requestFullscreen) {
-        video.requestFullscreen()
-      } else if (video.mozRequestFullScreen) {
-        video.mozRequestFullScreen()
-      } else if (video.webkitRequestFullscreen) {
-        video.webkitRequestFullscreen()
-      }
-      $('#fullscreen-btn').attr('fill', '#0095ff').attr('stroke', '#0095ff')
-      $('#cam-btn').hide('slow')
-    } else {
-      if (video.requestFullscreen) {
-        document.exitFullscreen()
-      } else if (video.mozRequestFullScreen) {
-        document.mozExitFullScreen()
-      } else if (video.webkitRequestFullscreen) {
-        document.webkitExitFullscreen()
-      }
-      $('#fullscreen-btn').attr('fill', '#fff').attr('stroke', '#fff')
-      $('#cam-btn').show('slow')
+const controls = {
+  enableFullscreen: video => {
+    if (video.requestFullscreen) {
+      video.requestFullscreen()
+    } else if (video.mozRequestFullScreen) {
+      video.mozRequestFullScreen()
+    } else if (video.webkitRequestFullscreen) {
+      video.webkitRequestFullscreen()
     }
-    $('#fullscreen-btn').find('path').toggle('slow')
-  })
-
-  $('#rec').click(function (e) {
-    if (!window.recorder) {
-      setupRecorder()
-    }
-    window.recorder.start(500)
-    $(this).hide()
-    $('#pause').show('slow')
-    disableToggle('#snap-btn', false)
+    $('#fullscreen-btn').attr('fill', '#0095ff').attr('stroke', '#0095ff')
     $('#cam-btn').hide('slow')
-  })
-
-  $('#pause').click(function () {
-    if (window.recorder.state === 'recording') {
-      $(this).attr('fill', '#0095ff').attr('stroke', '#0095ff')
-      window.recorder.pause()
-    } else if (window.recorder.state === 'paused') {
-      $(this).attr('fill', '#fff').attr('stroke', '#fff')
-      window.recorder.resume()
-    } else if ($('#local-video').prop('srcObject') === null) {
-      $('#local-video')[0].pause()
-      $(this).hide('slow')
-      $('#play').show('slow')
+  },
+  disableFullscreen: video => {
+    if (video.requestFullscreen) {
+      document.exitFullscreen()
+    } else if (video.mozRequestFullScreen) {
+      document.mozExitFullScreen()
+    } else if (video.webkitRequestFullscreen) {
+      document.webkitExitFullscreen()
     }
-  })
-  $('#play').click(function () {
-    $('#pause').attr('fill', '#fff').attr('stroke', '#fff').show('slow')
-    $(this).hide('slow')
-    $('#local-video')[0].play()
-  })
-
-  $('#stop-btn').click(function () {
-    if (window.recorder && window.recorder.state !== 'inactive') {
-      window.recorder.stop()
-    } else if ($('#local-video')[0].paused || $('#local-video')[0].playing) {
-      $('#local-video')[0].stop()
-    }
-  })
-  $('#cam-btn').click(async function (e) {
-    if (!window.stream) {
-      await navigator.mediaDevices
-        .getUserMedia({
-          video: true
-        })
-        .then(stream => {
-          window.stream = stream
-          $('#local-video').prop('srcObject', window.stream)
-        })
-        .catch(handleError)
-    }
-    if (e.target.innerHTML === 'Show Cam') {
-      $(this).html('Hide Cam')
-      disableToggle('#snap-btn', false)
-      disableToggle('#fullscreen-btn', false)
-      window.stream.getVideoTracks()[0].enabled = true
-    } else {
-      $(this).html('Show Cam')
-      disableToggle('#snap-btn', true)
-      disableToggle('#fullscreen-btn', true)
-      window.stream.getVideoTracks()[0].enabled = false
-    }
-  })
-  $('#setting-btn').click(function () {
-    $('#settings').slideToggle()
-  })
-  $('#snap-btn').click(e => {
+    $('#fullscreen-btn').attr('fill', '#fff').attr('stroke', '#fff')
+    $('#cam-btn').show('slow')
+  },
+  toggleFullscreenBtn: () => {
+    $('#fullscreen-btn').find('path').toggle('slow')
+  },
+  takeSnapShot: () => {
     const type = $('#image-type').val()
     const video = $('#local-video')
     const canvas = $('#preview-canvas')
@@ -165,8 +101,113 @@ $(document).ready(() => {
     video.hide()
     canvas.show()
     const url = canvas[0].toDataURL(`image/${type}`, 1.0)
-    downloadLinkSetup(url, type, 'Photo')
-  })
+    controls.setupDownloadLink(url, type, 'Image')
+  },
+  setupDownloadLink: (url, ext, type) => {
+    $('#download-btn')
+      .html(`Save ${type}`)
+      .prop('href', url)
+      .prop('download', `test.${ext}`)
+      .show('slow')
+  }
+}
+
+const media = {
+  init: () => {
+    $('#local-video')[0].onended = () => {
+      $('#play').show('slow')
+      $('#pause').hide('slow')
+    }
+    $('#local-video')[0].onpause = () => {
+      $('#play').show('slow')
+      $('#pause').hide('slow')
+    }
+    $('#local-video')[0].onplay = e => {
+      if ($('#local-video').prop('srcObject') === null) {
+        $('#pause').show('slow')
+      }
+      $('#play').hide('slow')
+    }
+  },
+  backToLiveFromSnap: () => {
+    $('#preview-canvas').hide()
+    $('#local-video').show()
+  },
+  backToLiveFromRec: () => {
+    $('#local-video').prop('srcObject', window.stream).prop('src', null)
+  }
+}
+
+const application = {
+  init: () => {
+    $('#fullscreen-btn').find('path:even').hide()
+    $('#pause').hide()
+    $('#play').hide()
+    $('#rec').hide()
+    $('#snap-btn').hide()
+    $('#fullscreen-btn').hide()
+    $('#stop').hide()
+    $('#rec').click(function (e) {
+      if (!window.recorder) {
+        recorder.init()
+      }
+      window.recorder.start(500)
+    })
+
+    $('#download-btn').click(function (e) {
+      $(this).hide('slow')
+      $(this).text().includes('Image')
+        ? media.backToLiveFromSnap()
+        : media.backToLiveFromRec()
+    })
+
+    $('#fullscreen-btn').click(function (e) {
+      const video = $('.video')[0]
+      if ($(this).attr('fill') === '#fff') {
+        controls.enableFullscreen(video)
+      } else {
+        controls.disableFullscreen(video)
+      }
+      controls.toggleFullscreenBtn()
+    })
+
+    $('#pause').click(function () {
+      if (window.recorder.state === 'recording') {
+        window.recorder.pause()
+      } else if (window.recorder.state === 'paused') {
+        window.recorder.resume()
+      } else if ($('#local-video').prop('srcObject') === null) {
+        $('#local-video')[0].pause()
+      }
+    })
+    $('#play').click(function () {
+      $('#local-video')[0].play()
+    })
+
+    $('#stop-btn').click(function () {
+      window.recorder.stop()
+    })
+    $('#cam-btn').click(async function (e) {
+      if (!window.stream) {
+        await stream.init()
+      }
+      if (e.target.innerHTML === 'Show Cam') {
+        stream.enableStream()
+      } else {
+        stream.disableStream()
+      }
+    })
+    $('#setting-btn').click(function () {
+      $('#settings').slideToggle()
+    })
+    $('#snap-btn').click(e => {
+      controls.takeSnapShot()
+    })
+  }
+}
+$(document).ready(() => {
+  application.init()
+  media.init()
 })
 // const green = () => {
 //   pixels = context.getImageData(0, 0, video.videoWidth, video.videoHeight)
